b683c4bb22c3f2afd76a4bb51a4c42dca69c3665
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-11-11 13:52:52 +0100

    allt klaaaart

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %We compute the intensity matrix Q (continuous time)
 %Then translate it using P=exp(Qt) 
 
-%clear all; clear workspace
+clear all; clear workspace
 % Parameters
 global lambda_1 lambda_2
 rng(1)
@@ -24,8 +24,10 @@ lambda_2=0.50;
 muvec_1 = [mu_AB mu_AC mu_C  mu_BC mu_A  mu_B mu_A];
 muvec_2 = [mu_C  mu_B  mu_AB mu_A  mu_BC mu_AC mu_B];
 
-%%
-Pi_matrix = []; %contains stationary distributions
+%% Analytic Solution
+
+%contains stationary distributions
+Pi_matrix = []; 
 
 for i=1:length(muvec_1)
     mu_1 = muvec_1(i);
@@ -36,56 +38,100 @@ for i=1:length(muvec_1)
     Q = getQmatrix(mu_1,mu_2);
     
     P = expm(Q);
-
     [V,D] = eig(P');
     Pi = V(:,1)';
     Pi = Pi/sum(Pi);
-    Pi_matrix(:,i) = Pi'; 
+    Pi_matrix_Jessika(:,i) = Pi'; 
+    
 end
 
 production_1 = d * Pi_matrix;
 
-%% Continuous time simulation
-% converges with 1% for T = 10000 vs. T = 50000
-T_max = 10000;
+%% Continuous time simulation inc. workers break
+
+% converges with 1% for T = 100000
+T_max = 100000; x = 0.6; 
+vec = [1,2,3,4];
+
 time_matrix = [];
+probability = [];
 for i=1:length(muvec_1)
     mu_1 = muvec_1(i);
     mu_2 = muvec_2(i);
-    state = 1; %Possible values, 1, 2, 3, 4.
-    state_time = [0 0 0 0];
-    vec = [1,2,3,4];
-    
+   
     %choose ONE of below
     %Q = bestQmatrix(mu_AB, mu_C);
     Q = getQmatrix(mu_1,mu_2);
-
-    t = 0;
+    
+    t = 0; 
+    state = datasample(vec,1);
+    state_time = [0 0 0 0];
+    both_working_visits = 0;
+    longer_than_x = 0;
+    
     while t < T_max
-        vec = [1,2,3,4];
-        zero = find(Q(state,:)==0);
-        vec([state zero]) = [];
         
-        X_1 = exprnd(1/Q(state,vec(1)));
-        X_2 = exprnd(1/Q(state,vec(2)));
-        [X,tmpstate] = min([X_1 X_2]);
+        T_min = Inf;
+        next = 0;
         
-        %update state
-        state = vec(tmpstate);
+        for tmp_state=1:length(Q)
+            
+            % We have to move. Look at neighbor states. Next.
+            if tmp_state == state
+                
+                continue;
+                
+            end
+                                    
+            % One of the states has q_{ij}=0. We cannot move here. Next.
+            if Q(state,tmp_state) == 0
+                
+                continue;
+                
+            end
+            
+            % Sample time
+            T_i = exprnd(1/Q(state,tmp_state));
+            
+            if T_i < T_min
+                
+                T_min = T_i;
+                next = tmp_state;
+                
+            end
+            
+        end   
+                
+        % Frequentist calc of prob longer than x    
+        if next == 1
+            
+            both_working_visits = both_working_visits + 1;
+            
+            if T_i > x
+                
+                longer_than_x = longer_than_x + 1;
+            
+            end
+            
+        end
         
-        %increment time
-        state_time(state) = state_time(state) + X;
-        t = t + 1;
+        % Update state
+        state_time(state) = state_time(state) + T_min;
+        t = t + T_min;
+        state = next;
+        
     end
     
-    % Sum over time in each state, not frequency
     time_matrix(:,i) = (state_time/sum(state_time))';
+    
+    probability = [probability longer_than_x/both_working_visits];
+
     
 end
    
 production_2 = d * time_matrix;
-
 %% Discretization-approach simulation
+
 N=10000000;
 h=0.001;
 t_max = h*N;
@@ -100,36 +146,33 @@ for i=1:length(muvec_1)
     Q = getQmatrix(mu_1,mu_2);
 
     I = eye(4);
-    P = I + Q*h + (Q*h).^2/2
-    P = expm(Q);
-    %sample initial state
+    P = I + Q*h; 
+    
     vec = [1,2,3,4];
     state = datasample(vec,1);
     state_count = [0 0 0 0];
-    tic
     t=0;
     
     while t < t_max
-        X = rand();
-        %compare random sample to probabilities p_{ij}
-        state = vec(state);
-        prob_vec = [];
         
+        state_count(state) = state_count(state) + 1;
+        X = rand();
+        
+        prob_vec = [];
         for k=1:4
             prob_vec(k) = sum(P(state,vec(1:k)));
         end
 
         stats = find(prob_vec > X);
-        j = stats(1);
-
-        state_count(j) = state_count(j) + 1;
+        state = stats(1);
         
         t=t+h;
+        
     end
     
     state_matrix(:,i) = (state_count/sum(state_count))';
 
 end
 
-production_3A = d * state_matrix;
+production_3 = d * state_matrix;
 
